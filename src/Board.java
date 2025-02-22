95f81a7033fcd1129023860016e8d4ce5c446e41
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2025-02-22 17:43:28 +0700

    feat: implement canPlacePiece, placePiece, and removePiece method

--- src/Board.java
+++ src/Board.java
@@ -30,24 +30,63 @@ public class Board {
         return grid;
     }
 
-    
-
     /* Method untuk memeriksa constraint penyimpanan Piece pada Board:
         1. Tidak menimpa piece lain
         2. Tidak keluar dari Board atau IndexOutOfBounds */ 
     public boolean canPlacePiece(Piece piece, int row, int col) {
+        // Dimensi Piece
+        char[][] pieceShape = piece.getShape();
+        int pieceRow = pieceShape.length;
+        int pieceCol = pieceShape[0].length;
 
+        // Pengecekan
+        for (int i = 0; i < pieceRow; i++) {
+            for (int j = 0; j < pieceCol; j++) {
+                // Tidak keluar dari Board atau 
+                if ((row + i > rows) || (col + j > cols)) {
+                    return false;
+                }
+                // Tidak menimpa piece lain
+                if ((pieceShape[i][j] != '.') && (grid[row + i][col + j] != '.')) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
     
     // Method untuk menyimpan Piece pada Board
     public void placePiece(Piece piece, int row, int col) {
+        // Dimensi Piece
+        char[][] pieceShape = piece.getShape();
+        int pieceRow = pieceShape.length;
+        int pieceCol = pieceShape[0].length;
 
+        // Menempatkan piece
+        for (int i = 0; i < pieceRow; i++) {
+            for (int j = 0; j < pieceCol; j++) {
+                if (pieceShape[i][j] != '.') {
+                    grid[row + i][col + j] = pieceShape[i][j];
+                }
+            }
+        }
     }
 
     // Method untuk menghapus Piece pada Board
     public void removePiece(Piece piece, int row, int col) {
+        // Dimensi Piece
+        char[][] pieceShape = piece.getShape();
+        int pieceRow = pieceShape.length;
+        int pieceCol = pieceShape[0].length;
 
+        // Menghapus piece
+        for (int i = 0; i < pieceRow; i++) {
+            for (int j = 0; j < pieceCol; j++) {
+                if (pieceShape[i][j] != '.') {
+                    grid[row + i][col + j] = '.';
+                }
+            }
+        }
     }
 
     // Method untuk mengecek apakah Board sudah penuh atau belum
