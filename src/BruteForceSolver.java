b20f5a5fedc39cbfff30f217323ecb6cb3d69218
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2025-02-22 19:46:41 +0700

    feat: add BruteForceSolver attribute and constructor

---
+++ src/BruteForceSolver.java
@@ -0,0 +1,17 @@
+import java.util.List; // Built-in interface list dari Java
+
+// Brute Force
+public class BruteForceSolver {
+    // Atribut Solver
+    private Board board;
+    private List<Piece> listOfPieces;
+
+    // Konstruktor Piece
+    public BruteForceSolver(Board board, List<Piece> listOfPieces) {
+        this.board = board;
+        this.listOfPieces = listOfPieces;
+    }
+    
+    /* Puzzle solver brute force */
+    
+}
