40edf8a7a565d705100e0d8bec944a49bc1bf431
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2025-02-23 16:19:19 +0700

    feat: add brute force algorithm with backtracking implementation

--- src/BruteForceSolver.java
+++ src/BruteForceSolver.java
@@ -5,13 +5,51 @@ public class BruteForceSolver {
     // Atribut Solver
     private Board board;
     private List<Piece> listOfPieces;
+    private int iterationCount = 0;
 
     // Konstruktor Piece
     public BruteForceSolver(Board board, List<Piece> listOfPieces) {
         this.board = board;
         this.listOfPieces = listOfPieces;
     }
+
+    // Getter: iterationCount
+    public int getIterationCount() {
+        return iterationCount;
+    }
     
-    /* Puzzle solver brute force */
+    /* Puzzle solver brute force: backtracking */
     
+    // Method bantuan untuk memulai backtacking
+    public void solve() {
+        backtrack(0);
+    }
+
+    private void backtrack(int index) {
+        // Menambah hitungan iterasi
+        iterationCount++;
+
+        // Basis: seluruh Piece sudah ditempatkan pada Board
+        if (index == listOfPieces.size()) {
+            return; 
+        }
+
+        // Brute force seluruh kemungkinan transformasi Piece dan posisi pada Board 
+        Piece piece = listOfPieces.get(index);
+        for (Piece transformedPiece : piece.getRotationsAndMirrors()) {;
+            for (int r = 0; r < board.getRows(); r++) {
+                for (int c = 0; c < board.getCols(); c++) {
+                    // Pengecekan penempatan piece selaku aturan IQ Puzzler Pro
+                    if (board.canPlacePiece(transformedPiece, r, c)) {
+                        board.placePiece(transformedPiece, r, c);
+                        // DFS
+                        backtrack(index + 1);
+                        // Rollback
+                        board.removePiece(transformedPiece, r, c);
+                    }
+                }
+            }
+        }
+    }
+
 }
