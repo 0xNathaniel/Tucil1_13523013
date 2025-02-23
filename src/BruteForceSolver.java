6ee0d8149228ae71c0c5f6bdfcc400c68827ef19
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2025-02-23 16:24:37 +0700

    refactor: backtracking stops as soon as it founds a solution

--- src/BruteForceSolver.java
+++ src/BruteForceSolver.java
@@ -25,31 +25,39 @@ public class BruteForceSolver {
         backtrack(0);
     }
 
-    private void backtrack(int index) {
+    private boolean backtrack(int idx) {
         // Menambah hitungan iterasi
         iterationCount++;
 
         // Basis: seluruh Piece sudah ditempatkan pada Board
-        if (index == listOfPieces.size()) {
-            return; 
+        if (idx == listOfPieces.size()) {
+            if (board.isFull()) {
+                System.out.println("Solution Found:");
+                board.printBoard();
+                return true;
+            }
+            return false; 
         }
 
         // Brute force seluruh kemungkinan transformasi Piece dan posisi pada Board 
-        Piece piece = listOfPieces.get(index);
+        Piece piece = listOfPieces.get(idx);
         for (Piece transformedPiece : piece.getRotationsAndMirrors()) {;
             for (int r = 0; r < board.getRows(); r++) {
                 for (int c = 0; c < board.getCols(); c++) {
                     // Pengecekan penempatan piece selaku aturan IQ Puzzler Pro
                     if (board.canPlacePiece(transformedPiece, r, c)) {
                         board.placePiece(transformedPiece, r, c);
-                        // DFS
-                        backtrack(index + 1);
-                        // Rollback
+                        // Melanjutkan proses searching (berhenti bila sudah menemukan solusi)
+                        if (backtrack(idx + 1)) {
+                            return true;
+                        }
+                        // Rollback: kembali pada level state-tree sebelumnya
                         board.removePiece(transformedPiece, r, c);
                     }
                 }
             }
         }
+        return false;
     }
 
 }
